f21385e223da7d04793ac6cf84a234c8cd060efd
Aki Väliaho <akivvaliaho@gmail.com>
2017-04-28 11:53:26 +0000

    Reflection dependency

---
+++ calculatorservice/src/main/java/com/akivaliaho/EventConf.java
@@ -0,0 +1,15 @@
+package com.akivaliaho;
+
+import com.akivaliaho.config.annotations.Interest;
+import com.akivaliaho.config.annotations.Interests;
+
+/**
+ * Created by vagrant on 4/28/17.
+ */
+@Interests(
+		interests = {
+				@Interest(value = "doSuperHardCalculation")
+		}
+)
+public class EventConf {
+}
