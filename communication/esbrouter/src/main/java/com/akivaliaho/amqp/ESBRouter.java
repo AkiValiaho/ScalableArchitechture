165446a912e9e24608d9c5b977b2f3b3fcbbd169
Aki Väliaho <akivvaliaho@gmail.com>
2017-06-18 17:23:31 +0300

    Preliminary splitting of a large configuration class

--- communication/esbrouter/src/main/java/com/akivaliaho/amqp/ESBRouter.java
+++ communication/esbrouter/src/main/java/com/akivaliaho/amqp/ESBRouter.java
@@ -1,28 +1,21 @@
 package com.akivaliaho.amqp;
 
-import com.akivaliaho.ConfigurationPollEventResult;
-import com.akivaliaho.RequestInterestedPartiesEvent;
 import com.akivaliaho.ServiceEvent;
-import com.akivaliaho.ServiceEventResult;
 import com.akivaliaho.config.ConfigEnum;
 import com.akivaliaho.config.ConfigurationHolder;
-import com.akivaliaho.event.AsyncQueue;
 import com.akivaliaho.event.InterestEvent;
 import com.akivaliaho.event.LocalEventDelegator;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.*;
-import org.springframework.amqp.rabbit.connection.CachingConnectionFactory;
-import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.core.Message;
+import org.springframework.amqp.core.MessageProperties;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
-import org.springframework.amqp.rabbit.listener.SimpleMessageListenerContainer;
-import org.springframework.amqp.rabbit.listener.adapter.MessageListenerAdapter;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
-import java.io.*;
-import java.util.Map;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
 import java.util.stream.Collectors;
 
 /**
@@ -33,48 +26,22 @@ import java.util.stream.Collectors;
 public class ESBRouter {
     private final ConfigurationHolder configurationHolder;
     private final LocalEventDelegator localEventDelegator;
-    private final AsyncQueue asyncQueue;
-    @Autowired
-    ApplicationContext applicationContext;
-    private String serviceName;
+    private final AmqpConfigurator amqpConfigurator;
+    private String toEsbExchange;
     private RabbitTemplate template;
-    private String routingKey;
-    private FanoutExchange mq_from_esb_exchange;
-    private TopicExchange mq_to_esb_exchange;
 
     @Autowired
-    public ESBRouter(ConfigurationHolder configurationHolder, LocalEventDelegator localEventDelegator, AsyncQueue asyncQueue) {
+    public ESBRouter(ConfigurationHolder configurationHolder, LocalEventDelegator localEventDelegator, AmqpConfigurator amqpConfigurator) {
         this.configurationHolder = configurationHolder;
         this.localEventDelegator = localEventDelegator;
-        this.asyncQueue = asyncQueue;
+        this.amqpConfigurator = amqpConfigurator;
     }
 
     @PostConstruct
     public void init() {
-        //TODO This initializer is too damn long and hard to read (not going to even try), do something about it.
-        Map<String, String> messagingConfiguration = configurationHolder.getMessagingConfiguration();
-        //Init connection to the master broker
-        CachingConnectionFactory connectionFactory = new CachingConnectionFactory(messagingConfiguration.get("mq.address"), Integer.parseInt(messagingConfiguration.get("mq.port")));
-        //Set username and password
-        connectionFactory.setUsername(messagingConfiguration.get("mq.username"));
-        connectionFactory.setPassword(messagingConfiguration.get("mq.password"));
-        // set up the queue, exchange, binding on the broker
-        RabbitAdmin admin = new RabbitAdmin(connectionFactory);
-        //Declare dedicated serviceQueue and toESBQueue
-        Queue serviceQueue = new Queue(messagingConfiguration.get(ConfigEnum.SERVICEQUEUE), true);
-        admin.declareQueue(serviceQueue);
-        Queue toESBQueue = new Queue(messagingConfiguration.get(ConfigEnum.MQ_TO_ESB_QUEUE), true);
-        mq_from_esb_exchange = new FanoutExchange(messagingConfiguration.get(ConfigEnum.MQ_FROM_ESB_ESCHANGE), true, false);
-        mq_to_esb_exchange = new TopicExchange(messagingConfiguration.get(ConfigEnum.MQ_TO_ESB_EXCHANGE), true, false);
-        bindingsInit(messagingConfiguration, admin, serviceQueue, toESBQueue);
-        messageContainerInit(connectionFactory, serviceQueue);
-        this.serviceName = messagingConfiguration.get("mq.servicebasename");
-        // send something
-        this.template = new RabbitTemplate(connectionFactory);
-
-        //Initialize localEventDelegatorlocalEventDelegator
-        EventUtil eventUtil = new EventUtil(this, asyncQueue);
-        localEventDelegator.setEventUtil(eventUtil);
+        amqpConfigurator.configureAmqp(this);
+        this.toEsbExchange = amqpConfigurator.getMq_to_esb_exchange();
+        this.template = amqpConfigurator.getTemplate();
         informAboutInterests();
     }
 
@@ -87,73 +54,6 @@ public class ESBRouter {
         routeEvent(new ServiceEvent(event));
     }
 
-    private void bindingsInit(Map<String, String> messagingConfiguration, RabbitAdmin admin, Queue serviceQueue, Queue toESBQueue) {
-        //TODO Bindings should be part of another AMQP-related configuration class
-        declare(admin, serviceQueue, toESBQueue, mq_from_esb_exchange, mq_to_esb_exchange);
-        routingKey = messagingConfiguration.get(ConfigEnum
-                .SERVICEBASENAME);
-        //Bind the incoming exchange to incoming queue
-        declareBindings(admin, serviceQueue, mq_from_esb_exchange);
-    }
-
-    private void messageContainerInit(CachingConnectionFactory connectionFactory, Queue serviceQueue) {
-        SimpleMessageListenerContainer container =
-                new SimpleMessageListenerContainer(connectionFactory);
-        Object listener = new Object() {
-            public void handleMessage(byte[] bytes) throws InstantiationException {
-                ByteArrayInputStream byteArrayInputStream = new ByteArrayInputStream(bytes);
-                try {
-                    ObjectInputStream objectInputStream = new ObjectInputStream(byteArrayInputStream);
-                    ServiceEvent serviceEvent = (ServiceEvent) objectInputStream.readObject();
-                    handleMessage(serviceEvent);
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                }
-            }
-
-            public void handleMessage(ServiceEvent foo) throws InstantiationException {
-                log.info("Got message: {}", foo.getEventName());
-                //Is it a ConfigurationPollResult?
-                if (foo instanceof ConfigurationPollEventResult) {
-                    localEventDelegator.delegateEvent(foo);
-                    return;
-                }
-                //Is it a request for the interested parties?
-                if (foo instanceof RequestInterestedPartiesEvent) {
-                    localEventDelegator.delegateEvent(foo);
-                    return;
-                }
-                //Is it a Result event?
-                if (foo.getEventName().toLowerCase().contains("result")) {
-                    asyncQueue.solveResult(((ServiceEventResult) foo));
-                } else {
-                    localEventDelegator.delegateEvent(foo);
-                }
-            }
-
-            public void handleMessage(Object object) {
-                //Something is wrong because this is not an event
-                log.error("Message received is malformed: {}", object);
-            }
-        };
-        MessageListenerAdapter adapter = new MessageListenerAdapter(listener);
-        container.setMessageListener(adapter);
-        container.setQueueNames(serviceQueue.getName());
-        container.start();
-    }
-
-    private void declareBindings(RabbitAdmin admin, Queue service_queue, FanoutExchange mq_from_esb_exchange) {
-        //TODO Declarations should be part of another composed class, this is a router after all
-        admin.declareBinding(
-                BindingBuilder.bind(service_queue).to(mq_from_esb_exchange));
-    }
-
-    private void declare(RabbitAdmin admin, Queue fromBrokerQueue, Queue toBrokerQueue, FanoutExchange masterBrokerIncomingExchange, TopicExchange masterBrokerOutboundExchange) {
-        admin.declareExchange(masterBrokerIncomingExchange);
-        admin.declareExchange(masterBrokerOutboundExchange);
-        admin.declareQueue(toBrokerQueue);
-        admin.declareQueue(fromBrokerQueue);
-    }
 
     public <V> V routeEvent(ServiceEvent event) {
         //Send message to an exchange which routes it to the camel esb
@@ -164,9 +64,9 @@ public class ESBRouter {
             ObjectOutputStream objectOutputStream = new ObjectOutputStream(bos);
             objectOutputStream.writeObject(event);
             MessageProperties messageProperties = new MessageProperties();
-            messageProperties.setHeader("serviceName", serviceName);
+            messageProperties.setHeader("serviceName", amqpConfigurator.getServiceName());
             Message message = new Message(bos.toByteArray(), messageProperties);
-            this.template.convertAndSend(mq_to_esb_exchange.getName(), "master", message);
+            this.template.convertAndSend(toEsbExchange, "master", message);
             return null;
         } catch (IOException e) {
             e.printStackTrace();
