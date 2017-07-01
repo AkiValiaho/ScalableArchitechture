e5272bef0a14b7b83574b49623b7d6df2abd1fd4
Aki Väliaho <akivvaliaho@gmail.com>
2017-07-01 21:40:24 +0300

    AmqpEventStrategyHolderTest added, some changes to the holder itself

--- communication/esbrouter/src/main/java/com/akivaliaho/amqp/eventstrategy/AmqpEventStrategyHolder.java
+++ communication/esbrouter/src/main/java/com/akivaliaho/amqp/eventstrategy/AmqpEventStrategyHolder.java
@@ -2,6 +2,9 @@ package com.akivaliaho.amqp.eventstrategy;
 
 import com.akivaliaho.ConfigurationPollEventResult;
 import com.akivaliaho.RequestInterestedPartiesEvent;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
@@ -13,27 +16,35 @@ import java.util.Map;
  * Created by vagrant on 6/21/17.
  */
 @Component
+@NoArgsConstructor
 public class AmqpEventStrategyHolder {
 
-    private final Strategy defaultServiceEventStrategy;
-    private final Strategy configStrategy;
-    private final Strategy requestStrategy;
+    @Getter
+    @Setter
+    private Strategy defaultServiceEventStrategy;
+    @Getter
+    @Setter
+    private Strategy configStrategy;
+    @Getter
+    @Setter
+    private Strategy requestStrategy;
+    @Getter
     private Map<String, Strategy> strategyMap;
 
     @Autowired
     public AmqpEventStrategyHolder(@Qualifier("defaultServiceEventStrategy") Strategy defaultServiceEventStrategy,
                                    @Qualifier("configurationPollEventResultStrategy") Strategy configurationPollEventResultStrategy,
-                                   @Qualifier("requestInterestedPartiesEventStrategy")Strategy requestInterestedPartiesEventStrategy) {
+                                   @Qualifier("requestInterestedPartiesEventStrategy") Strategy requestInterestedPartiesEventStrategy) {
         this.defaultServiceEventStrategy = defaultServiceEventStrategy;
         this.configStrategy = configurationPollEventResultStrategy;
-    this.requestStrategy = requestInterestedPartiesEventStrategy;
+        this.requestStrategy = requestInterestedPartiesEventStrategy;
         initStrategies();
     }
 
     public void initStrategies() {
         strategyMap = new HashMap<>();
         strategyMap.put(ConfigurationPollEventResult.class.getCanonicalName(), configStrategy);
-        strategyMap.put(RequestInterestedPartiesEvent.class.getCanonicalName(),requestStrategy);
+        strategyMap.put(RequestInterestedPartiesEvent.class.getCanonicalName(), requestStrategy);
     }
 
     public Strategy getStrategy(String canonicalName) {
--- communication/esbrouter/src/test/java/com/akivaliaho/amqp/eventstrategy/AmqpEventStrategyHolderTest.java
+++ communication/esbrouter/src/test/java/com/akivaliaho/amqp/eventstrategy/AmqpEventStrategyHolderTest.java
@@ -1,24 +1,60 @@
 package com.akivaliaho.amqp.eventstrategy;
 
+import com.akivaliaho.ConfigurationPollEventResult;
+import com.akivaliaho.RequestInterestedPartiesEvent;
+import org.junit.Before;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.Mockito.mock;
 
 /**
  * Created by vagrant on 6/21/17.
  */
 public class AmqpEventStrategyHolderTest {
+    private AmqpEventStrategyHolder amqpEventStrategyHolder;
+    private Strategy defaultServiceEventStrategy;
+    private Strategy configurationPollStrategy;
+    private Strategy requestInterestedPartiesStrategy;
 
-    //TODO Write test
+
+    @Before
+    public void before() {
+        defaultServiceEventStrategy = mock(DefaultServiceEventStrategy.class);
+        configurationPollStrategy = mock(ConfigurationPollEventResultStrategy.class);
+        requestInterestedPartiesStrategy = mock(RequestInterestedPartiesEventStrategy.class);
+        this.amqpEventStrategyHolder = new AmqpEventStrategyHolder();
+    }
+
+    @Test
+    public void constructorTest() {
+        AmqpEventStrategyHolder amqpEventStrategyHolder = new AmqpEventStrategyHolder(mock(Strategy.class), mock(Strategy.class), mock(Strategy.class));
+        //Should call init strategies
+        assertTrue(amqpEventStrategyHolder.getStrategyMap().size() == 2);
+    }
 
     @Test
     public void initStrategies() throws Exception {
+        amqpEventStrategyHolder.initStrategies();
+        assertTrue(amqpEventStrategyHolder.getStrategyMap().size() == 2);
     }
 
     @Test
     public void getStrategy() throws Exception {
+        //DefaultServiceEventStrategy
+        this.amqpEventStrategyHolder = new AmqpEventStrategyHolder(defaultServiceEventStrategy, configurationPollStrategy, requestInterestedPartiesStrategy);
+        Strategy<Object> strategy = this.amqpEventStrategyHolder.getStrategy();
+        assertEquals(strategy, defaultServiceEventStrategy);
     }
 
     @Test
-    public void getStrategy1() throws Exception {
+    public void getStrategyAny() throws Exception {
+        this.amqpEventStrategyHolder = new AmqpEventStrategyHolder(defaultServiceEventStrategy, configurationPollStrategy, requestInterestedPartiesStrategy);
+        Strategy configurationPollStrategyReturned = this.amqpEventStrategyHolder.getStrategy(ConfigurationPollEventResult.class.getCanonicalName());
+        assertEquals(configurationPollStrategy, configurationPollStrategyReturned);
+        Strategy requestInterestedPartiesReturned = this.amqpEventStrategyHolder.getStrategy(RequestInterestedPartiesEvent.class.getCanonicalName());
+        assertEquals(requestInterestedPartiesStrategy, requestInterestedPartiesReturned);
     }
 
 }
